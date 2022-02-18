a1309be7132f20c4c28d3dabbbae806bea74ac76
Bammmmmm <155812@ettyhillesumlyceum.nl>
2022-02-18 11:36:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-enhupx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-enhupx)
---
+++ src/app/dierenprofiel/dierenprofiel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dierenprofiel',
+  templateUrl: './dierenprofiel.component.html',
+  styleUrls: ['./dierenprofiel.component.css']
+})
+export class DierenprofielComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
